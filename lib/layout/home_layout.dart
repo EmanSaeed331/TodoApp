c1a4407b060c521a5a0a9ceb2ae296a11e497b77
EmanSaeed331 <emansaeed5330@gmail.com>
2021-09-12 07:44:42 +0200

    added Components.dart

--- lib/layout/home_layout.dart
+++ lib/layout/home_layout.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutterprojects/modules/archived_tasks/archived_tasks_screen.dart';
 import 'package:flutterprojects/modules/done_tasks/done_tasks_screen.dart';
 import 'package:flutterprojects/modules/new_tasks/new_tasks_screen.dart';
-
+import 'package:flutterprojects/shared/Components/Components.dart';
+import 'package:intl/intl.dart';
+import 'package:sqflite/sqflite.dart';
 class HomeLayout extends StatefulWidget {
   const HomeLayout({Key? key}) : super(key: key);
 
@@ -10,22 +12,40 @@ class HomeLayout extends StatefulWidget {
   State<HomeLayout> createState() => _HomeLayoutState();
 }
 
-class _HomeLayoutState extends State<HomeLayout> {
+class _HomeLayoutState extends State<HomeLayout>
+{
   int currentIndex=0;
+  IconData fabIcon = Icons.edit;
+
   List <Widget> Screens = [
     NewTasksScreen(),
     DoneTasksScreen(),
     ArchivedTasksScreen(),
 
   ];
+  var TextController = TextEditingController();
+  var TimeController = TextEditingController();
+  var DateController = TextEditingController();
+
+
+  bool bottomsheetshown = false;
   List <String> titles = [
     'New Tasks',
     'Done Tasks',
     'Archived Tasks',
   ];
+  late Database database;
+  var  scaffoldkey = GlobalKey <ScaffoldState>();
+  var  formkey = GlobalKey <FormState>();
   @override
+  void initState() {
+    super.initState();
+    CreateDatabase();
+  }
+    @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold
+      (key:scaffoldkey,
       appBar:AppBar(
       title:
         Text(
@@ -34,9 +54,139 @@ class _HomeLayoutState extends State<HomeLayout> {
       ),
       body:Screens[currentIndex],
       floatingActionButton :FloatingActionButton(
-        onPressed: (){},
+        onPressed: (){
+          if(bottomsheetshown)
+            InsertToDatabase(
+              title:TextController.text,
+              time :TimeController.text,
+              Date :DateController.text,
+
+            ).then((value) => {
+            Navigator.pop(context),
+                bottomsheetshown = false,
+                setState((){
+            fabIcon = Icons.edit;})
+
+          });
+
+
+
+
+
+
+
+
+          else
+            {
+              setState((){
+                fabIcon = Icons.add;
+
+              });
+            scaffoldkey.currentState?.showBottomSheet (
+              (context) => Container(
+              color:Colors.white,
+
+              padding:EdgeInsets.all(20.0),
+              child: Form(
+                key : formkey,
+                child: Column(
+                  mainAxisSize:MainAxisSize.min ,
+                  children:
+                  [ TextFormField(
+                  controller: TextController,
+                  keyboardType:TextInputType.text,
+                  //onFieldSubmitted: onSubmit,
+                  //onChanged: onchange,
+                  onTap: ((){
+                    print('TASK TAPPED');
+                  }),
+                  // validator: validate,
+                  decoration: InputDecoration(
+                    labelText: 'New Task',
+                    prefixIcon: Icon(Icons.title),
+                    border: OutlineInputBorder(),
+
+                  ),
+                  ),
+                    SizedBox(
+                      height:15,
+                    ),
+                    TextFormField(
+                      controller: TimeController,
+                      keyboardType:TextInputType.text,
+                      //onFieldSubmitted: onSubmit,
+                      //onChanged: onchange,
+                        decoration: InputDecoration(
+                          labelText: 'Time Task',
+                          prefixIcon: Icon(Icons.watch_later_outlined),
+                          border: OutlineInputBorder(),
+
+                        ),
+
+                      onTap: (){
+                        showTimePicker(context: context,
+                          initialTime: TimeOfDay.now(),).then((dynamic value)  {
+                            print(value.format(context).toString());
+
+                             TimeController.text = value.format(context).toString();
+
+
+
+                        });
+                        }
+                      // validator: validate,
+
+                    ),
+                    SizedBox(
+                      height:15,
+                    ),
+                    TextFormField(
+                        controller: DateController,
+                        keyboardType:TextInputType.text,
+                        //onFieldSubmitted: onSubmit,
+                        //onChanged: onchange,
+                        decoration: InputDecoration(
+                          labelText: 'Date Task',
+                          prefixIcon: Icon(Icons.calendar_today),
+                          border: OutlineInputBorder(),
+
+                        ),
+
+                        onTap: (){
+
+                              showDatePicker(context: context,
+                                  initialDate: DateTime.now(),
+                                  firstDate: DateTime.now(),
+                                  lastDate: DateTime.parse('2022-05-03')).
+                                  then((value) {
+                                    DateController.text = DateFormat.yMMMd().format(value!);
+
+                              } );
+
+
+                        }
+                      // validator: validate,
+
+                    ),
+
+                  ],
+
+
+                    ),
+              ),
+
+            ),
+              elevation: 20.0,
+
+            );
+            bottomsheetshown = true;
+
+            }
+
+
+        },
         child:
-        Icon(Icons.add
+        Icon(fabIcon
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -73,4 +223,47 @@ class _HomeLayoutState extends State<HomeLayout> {
       ),
     );
   }
+
+
+   CreateDatabase () async  {
+      database = await openDatabase(
+      'Todo.db',
+      version :1,
+      onCreate  :  (database,version)
+      {
+        print('database Created');
+         database.execute('CREATE TABLE tasks (id INTEGER PRIMARY KEY,title TEXT,Date TEXT ,Time TEXT , Status TEXT )').then((Value){
+           print('Table is created');
+         }).catchError((error){
+           print('Error ${error.toString()}');
+         });
+      },
+      onOpen:(database){
+        print('databse Opened');
+      },
+    ) as Database;
+  }
+
+  Future InsertToDatabase ({
+    required String title,
+    required String time,
+    required String Date,
+  }) async
+  {
+     await database.transaction(( txn)
+    {
+      return txn.rawInsert('INSERT INTO tasks (title,Date,Time,Status) VALUES ("$title" ,"$time","$Date","New")').then((value) {
+          print('Inserted Successfully ${value}');
+        }).catchError((error)
+        {
+          print('ERROR ${error.toString()}');
+
+        });
+
+
+    });
+
+
+  }
+
 }
---
+++ lib/shared/Components/Components.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+Widget defaultButton({
+  double width = double.infinity,
+  Color background = Colors.blue,
+  bool isUpperCase = true,
+  double radius = 3.0,
+  required Function function,
+  required String text,
+}) =>
+    Container(
+      width: width,
+      height: 50.0,
+      child: MaterialButton(
+      //  onPressed: function,
+        onPressed: () {  },
+        child: Text(
+          isUpperCase ? text.toUpperCase() : text,
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(
+          radius,
+        ),
+        color: background,
+      ),
+    );
+
+Widget defaultFormField({
+  required TextEditingController controller,
+  required TextInputType type,
+  //required final VoidCallback onSubmit,
+ // required final VoidCallback onchange,
+  required final VoidCallback ontap,
+  bool isPassword = false,
+  //required final VoidCallback validate,
+  required String label,
+  required IconData prefix,
+  IconData ?suffix,
+  final VoidCallback? suffixPressed,
+  bool isClickable = true,
+}) =>
+    TextFormField(
+      controller: controller,
+      keyboardType: type,
+      obscureText: isPassword,
+      enabled: isClickable,
+      //onFieldSubmitted: onSubmit,
+      //onChanged: onchange,
+      onTap: ontap,
+     // validator: validate,
+      decoration: InputDecoration(
+        labelText: label,
+        prefixIcon: Icon(
+          prefix,
+        ),
+        suffixIcon: suffix != null
+            ? IconButton(
+          onPressed: suffixPressed,
+          icon: Icon(
+            suffix,
+          ),
+        )
+            : null,
+        border: OutlineInputBorder(),
+      ),
+    );
